5c590dedbc1dc910aa77663870a4ea2f2fa12798
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-27 14:10:12 -0300

    feat: adiciona configuração de entidade para Produto

---
+++ src/HeroDotNet.Data/HeroContext/HeroContextDb.cs
@@ -0,0 +1,17 @@
+﻿using HeroDotNet.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeroDotNet.Data.HeroContext;
+
+internal class HeroContextDb(DbContextOptions<HeroContextDb> opt) : DbContext(opt)
+{
+    public DbSet<Produto> Produtos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Produto>().ToTable("PRODUTO");
+        modelBuilder.Entity<Produto>().HasKey(p => p.Id);
+        modelBuilder.Entity<Produto>().Property(p => p.NomeProduto).HasMaxLength(30).IsRequired();
+        modelBuilder.Entity<Produto>().Property(p => p.DataCriacao).IsRequired();
+        modelBuilder.Entity<Produto>().Property(p => p.DataAlteracao);
+    }
+}
