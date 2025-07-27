16cf2e911266f426afb6839abd154a7f6ec24096
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-27 14:09:48 -0300

    refactor: altera tipo ProdutoId para TbProdutoId com mais opções de operators

--- src/HeroDotNet.Domain/Entity/Produto.cs
+++ src/HeroDotNet.Domain/Entity/Produto.cs
@@ -1,17 +1,6 @@
-﻿namespace HeroDotNet.Domain.Entity;
+﻿using HeroDotNet.Domain.Core;
 
-public readonly struct ProdutoId
-{
-    public Guid Value { get; }
-
-    public ProdutoId(Guid value) => Value = value;
-
-    public static ProdutoId NewGuid() => new(Guid.NewGuid());
-    public override string ToString() => Value.ToString();
-
-    public static implicit operator Guid(ProdutoId id) => id.Value;
-    public static explicit operator ProdutoId(Guid id) => new(id);
-}
+namespace HeroDotNet.Domain.Entity;
 
 public sealed class Produto
 {
@@ -27,7 +16,7 @@ public sealed class Produto
         DataAlteracao = DateTime.UtcNow;
     }
 
-    public ProdutoId Id { get; }
+    public TbProdutoId Id { get; }
     public string? NomeProduto { get; private set; }
     public DateTime DataCriacao { get; private set; }
     public DateTime? DataAlteracao { get; private set; }
---
+++ src/HeroDotNet.Domain/IRepository/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using HeroDotNet.Domain.Core;
+using HeroDotNet.Domain.Entity;
+
+namespace HeroDotNet.Domain.IRepository;
+
+public interface IProdutoRepository
+{
+    Task AdicionarProduto(Produto produto);
+    Task AdicionarProdutos(Produto[] produtos);
+    Task<Produto?> ObterProdutoPorId(TbProdutoId produtoId);
+    Task<Produto[]> ObterProdutosPorNome(string nomeProduto);
+}
