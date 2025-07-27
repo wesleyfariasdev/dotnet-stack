33a112ab80100134a16eba88ae8d26c5cbc753b3
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-27 10:57:13 -0300

    Adicionar arquivos de projeto.

---
+++ src/HeroDotNet.Domain/Entity/Produto.cs
@@ -0,0 +1,34 @@
+﻿namespace HeroDotNet.Domain.Entity;
+
+public readonly struct ProdutoId
+{
+    public Guid Value { get; }
+
+    public ProdutoId(Guid value) => Value = value;
+
+    public static ProdutoId NewGuid() => new(Guid.NewGuid());
+    public override string ToString() => Value.ToString();
+
+    public static implicit operator Guid(ProdutoId id) => id.Value;
+    public static explicit operator ProdutoId(Guid id) => new(id);
+}
+
+public sealed class Produto
+{
+    public Produto(string? nomeProduto)
+    {
+        NomeProduto = nomeProduto;
+        DataCriacao = DateTime.UtcNow;
+    }
+
+    public void AlterarNomeProduto(string nomeProduto)
+    {
+        NomeProduto = nomeProduto;
+        DataAlteracao = DateTime.UtcNow;
+    }
+
+    public ProdutoId Id { get; }
+    public string? NomeProduto { get; private set; }
+    public DateTime DataCriacao { get; private set; }
+    public DateTime? DataAlteracao { get; private set; }
+}
