97eb19d8c28ad1d1f2cf4affca03d619a582d8e7
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-27 14:13:01 -0300

    feat: adiciona Decoractor Pattern ao Services para Redis

---
+++ src/HeroDotNet.Application/Services/ProdutoRepositoryCacheDecorator.cs
@@ -0,0 +1,29 @@
+﻿using HeroDotNet.Application.Services.IServices;
+using HeroDotNet.Domain.Core;
+using HeroDotNet.Domain.Entity;
+using HeroDotNet.Domain.IRepository;
+
+namespace HeroDotNet.Application.Services;
+
+internal class ProdutoRepositoryCacheDecorator(IProdutoRepository produtoRepository) : IProdutoServices
+{
+    public Task AdicionarProduto(Produto produto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AdicionarProdutos(Produto[] produtos)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Produto?> ObterProdutoPorId(TbProdutoId produtoId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Produto[]> ObterProdutosPorNome(string nomeProduto)
+    {
+        throw new NotImplementedException();
+    }
+}
