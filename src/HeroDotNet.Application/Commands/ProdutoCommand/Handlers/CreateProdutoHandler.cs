fa0310d6d45a039bae0e7a8f8355cc2800d78e02
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-28 21:16:27 -0300

    feat: adiciona unitofwork

--- src/HeroDotNet.Application/Commands/ProdutoCommand/Handlers/CreateProdutoHandler.cs
+++ src/HeroDotNet.Application/Commands/ProdutoCommand/Handlers/CreateProdutoHandler.cs
@@ -5,12 +5,13 @@ using MediatR;
 
 namespace HeroDotNet.Application.Commands.ProdutoCommand.Handlers;
 
-public class CreateProdutoHandler(IProdutoRepository produtoRepository) : IRequestHandler<CreateProdutoCommand, TbProdutoId>
+public class CreateProdutoHandler(IProdutoRepository produtoRepository, IUnitOfWork unitOfWork) : IRequestHandler<CreateProdutoCommand, TbProdutoId>
 {
     public async Task<TbProdutoId> Handle(CreateProdutoCommand request, CancellationToken cancellationToken)
     {
         var produto = new Produto(request.NomeProduto);
         await produtoRepository.AdicionarProduto(produto);
+        await unitOfWork.CommitAsync();
         return produto.Id;
     }
 }
