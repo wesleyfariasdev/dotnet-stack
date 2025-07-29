cc417154895affc7a1d85b2d290ba79b0513aa92
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-28 21:16:44 -0300

    feat: adiciona produto Response

---
+++ src/HeroDotNet.Application/Dto/ProductResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using HeroDotNet.Domain.Core;
+
+namespace HeroDotNet.Application.Dto;
+
+public sealed class ProductResponseDto
+{
+    public TbProdutoId Id { get; set; }
+    public string? NomeProduto { get; set; }
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataAlteracao { get; set; }
+}
