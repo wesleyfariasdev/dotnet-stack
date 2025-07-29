c566786980bb042cc677bec04422b295250e354d
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-28 23:35:44 -0300

    feat: implementa camada de Db e Services

---
+++ src/HeroDotNet.Infraestructure/Db/DbServices.cs
@@ -0,0 +1,23 @@
+﻿using HeroDotNet.Data.HeroContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HeroDotNet.Infraestructure.Db;
+
+public static class DbServices
+{
+    public static IServiceCollection AddDbConfig(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config["ConnectionStrings:DefaultConnection"];
+
+        if (string.IsNullOrEmpty(connectionString))
+            connectionString = config.GetConnectionString("DEV");
+
+        services.AddDbContext<HeroContextDb>(q => q.UseSqlServer(connectionString,
+                                                  q => q.MigrationsAssembly(typeof(HeroContextDb)
+                                                        .Assembly.FullName)));
+
+        return services;
+    }
+}
---
+++ src/HeroDotNet.Infraestructure/Services/InfraServices.cs
@@ -0,0 +1,20 @@
+﻿using HeroDotNet.Application.Services;
+using HeroDotNet.Data.Repository;
+using HeroDotNet.Domain.IRepository;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace HeroDotNet.Infraestructure.Services;
+
+public static class InfraServices
+{
+    public static IServiceCollection AddInfraServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<ProdutoRepository>();
+        services.AddScoped<IProdutoRepository>(sp => new ProdutoRepositoryCacheDecorator
+                                              (sp.GetRequiredService<ProdutoRepository>(),
+                                               sp.GetRequiredService<IDatabase>()));
+        return services;
+    }
+}
