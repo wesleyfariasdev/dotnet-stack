36b884c12000cfe917db8caf2cb348fe6551e69d
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-29 22:34:37 -0300

    feat: configuração da entidade de produto

--- src/HeroDotNet.Data/HeroContext/HeroContextDb.cs
+++ src/HeroDotNet.Data/HeroContext/HeroContextDb.cs
@@ -1,5 +1,7 @@
-﻿using HeroDotNet.Domain.Entity;
+﻿using HeroDotNet.Domain.Core;
+using HeroDotNet.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HeroDotNet.Data.HeroContext;
 
@@ -8,10 +10,21 @@ public class HeroContextDb(DbContextOptions<HeroContextDb> opt) : DbContext(opt)
     public DbSet<Produto> Produtos { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        var converterProdutoId = new ValueConverter<TbProdutoId, Guid>
+                                     (v => v, v => TbProdutoId.FromGuid(v));
+
         modelBuilder.Entity<Produto>().ToTable("PRODUTO");
         modelBuilder.Entity<Produto>().HasKey(p => p.Id);
-        modelBuilder.Entity<Produto>().Property(p => p.NomeProduto).HasMaxLength(30).IsRequired();
-        modelBuilder.Entity<Produto>().Property(p => p.DataCriacao).IsRequired();
-        modelBuilder.Entity<Produto>().Property(p => p.DataAlteracao);
+        modelBuilder.Entity<Produto>().Property(p => p.Id).HasConversion(converterProdutoId)
+                                                          .ValueGeneratedNever()
+                                                          .HasColumnName("ID");
+
+        modelBuilder.Entity<Produto>().Property(p => p.NomeProduto).HasMaxLength(30)
+                                                                   .IsRequired()
+                                                                   .HasColumnName("NOME_PRODUTO");
+
+        modelBuilder.Entity<Produto>().Property(p => p.DataCriacao).IsRequired()
+                                                                   .HasColumnName("DT_CRIACAO");
+        modelBuilder.Entity<Produto>().Property(p => p.DataAlteracao).HasColumnName("DT_ALTERACAO");
     }
 }
