c94d9aac3168b0e1552d9c583da60a91a5ef0e0f
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-30 02:07:43 -0300

    feat: adiciona método para ler e escrever Guids no padrão TbProdutoId

---
+++ src/HeroDotNet.Domain/Core/TbProdutoIdJsonConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace HeroDotNet.Domain.Core;
+
+public class TbProdutoIdJsonConverter : JsonConverter<TbProdutoId>
+{
+    public override TbProdutoId Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => TbProdutoId.FromGuid(Guid.Parse(reader.GetString()!));
+
+    public override void Write(Utf8JsonWriter writer, TbProdutoId value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value.ToString());
+}
