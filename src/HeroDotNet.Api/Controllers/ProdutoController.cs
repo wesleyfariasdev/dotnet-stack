229c69926fd1b99f73d5c4de551593ebb899cfa3
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-29 22:12:25 -0300

    feat: adiciona api para produto

---
+++ src/HeroDotNet.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using HeroDotNet.Application.Commands.ProdutoCommand;
+using HeroDotNet.Application.Queries.ProdutoQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroDotNet.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutoController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateProduto([FromBody] CreateProdutoCommand command)
+    {
+        if (command == null)
+        {
+            return BadRequest("Invalid product data.");
+        }
+        var result = await mediator.Send(command);
+        return CreatedAtAction(nameof(GetProdutoByIdQuery), new { id = result.ToString() });
+
+        //if (res)
+        //{
+        //    return CreatedAtAction(nameof(GetProdutoById), new { id = result.Value.Id }, result.Value);
+        //}
+        //return BadRequest(result.ErrorMessage);
+    }
+}
