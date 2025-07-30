32080eeb6813982ab38512b7780e1fbcc6996b86
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-30 02:06:57 -0300

    chore: adiciona método para buscar produto por Id

--- src/HeroDotNet.Api/Controllers/ProdutoController.cs
+++ src/HeroDotNet.Api/Controllers/ProdutoController.cs
@@ -1,5 +1,6 @@
 ﻿using HeroDotNet.Application.Commands.ProdutoCommand;
 using HeroDotNet.Application.Queries.ProdutoQueries;
+using HeroDotNet.Domain.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,19 +11,28 @@ namespace HeroDotNet.Api.Controllers;
 public class ProdutoController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
+    [HttpPost]
     public async Task<IActionResult> CreateProduto([FromBody] CreateProdutoCommand command)
     {
         if (command == null)
-        {
             return BadRequest("Invalid product data.");
-        }
-        var result = await mediator.Send(command);
-        return CreatedAtAction(nameof(GetProdutoByIdQuery), new { id = result.ToString() });
 
-        //if (res)
-        //{
-        //    return CreatedAtAction(nameof(GetProdutoById), new { id = result.Value.Id }, result.Value);
-        //}
-        //return BadRequest(result.ErrorMessage);
+        var result = await mediator.Send(command);
+
+        return CreatedAtAction(
+            nameof(GetProdutoById),
+            new { id = result.ToString() },
+            result
+        );
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProdutoById(Guid id)
+    {
+        var produto = await mediator.Send(new GetProdutoByIdQuery(TbProdutoId.FromGuid(id)));
+        if (produto == null)
+            return NotFound();
+
+        return Ok(produto);
     }
 }
