ec5ba8026f18ae79c0c01bc1a933df4806e87814
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-30 02:26:01 -0300

    feat: adiciona projeto para testes com Xunit

---
+++ test/HeroDotNet.Domain.Test/ProdutoTest.cs
@@ -0,0 +1,26 @@
+﻿using HeroDotNet.Domain.Entity;
+
+namespace HeroDotNet.Domain.Test;
+
+public class ProdutoTest
+{
+    [Fact]
+    public void CriaProdutoComSucessoTest()
+    {
+        var produto = new Produto("Produto Teste");
+        Assert.NotNull(produto.Id);
+        Assert.Equal("Produto Teste", produto.NomeProduto);
+        Assert.NotNull(produto.DataCriacao);
+        Assert.Null(produto.DataAlteracao);
+    }
+
+    [Fact]
+    public void AlteraNomeProdutoComSucessoTest()
+    {
+        var produto = new Produto("Produto Teste");
+        produto.AlterarNomeProduto("Produto Teste Alterado");
+        Assert.Equal("Produto Teste Alterado", produto.NomeProduto);
+        Assert.NotNull(produto.DataAlteracao);
+        Assert.True(produto.DataAlteracao > produto.DataCriacao);
+    }
+}
