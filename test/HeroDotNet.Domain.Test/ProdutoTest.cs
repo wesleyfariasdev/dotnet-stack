aa4280b6140b092faadda3d0e8ddb2d5d083ab9c
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-07-30 02:55:06 -0300

    test: adiciona cobertura de teste

--- test/HeroDotNet.Domain.Test/ProdutoTest.cs
+++ test/HeroDotNet.Domain.Test/ProdutoTest.cs
@@ -5,17 +5,17 @@ namespace HeroDotNet.Domain.Test;
 public class ProdutoTest
 {
     [Fact]
-    public void CriaProdutoComSucessoTest()
+    public void Cria_Produto_Com_Sucesso_Test()
     {
         var produto = new Produto("Produto Teste");
-        Assert.NotNull(produto.Id);
+        Assert.False(string.IsNullOrWhiteSpace(produto.Id.ToString()));
         Assert.Equal("Produto Teste", produto.NomeProduto);
-        Assert.NotNull(produto.DataCriacao);
+        Assert.True(produto.DataCriacao < DateTime.UtcNow);
         Assert.Null(produto.DataAlteracao);
     }
 
     [Fact]
-    public void AlteraNomeProdutoComSucessoTest()
+    public void Altera_Nome_Produto_Com_Sucesso_Test()
     {
         var produto = new Produto("Produto Teste");
         produto.AlterarNomeProduto("Produto Teste Alterado");
@@ -23,4 +23,55 @@ public class ProdutoTest
         Assert.NotNull(produto.DataAlteracao);
         Assert.True(produto.DataAlteracao > produto.DataCriacao);
     }
+
+    [Fact]
+    public void CriarProduto_ComNomeNulo_DeveLancarExcecao_Teste() =>
+        Assert.Throws<ArgumentException>(() => new Produto(null));
+
+    [Fact]
+    public void CriarProduto_ComNomeVazio_DeveLancarExcecao_Teste() =>
+        Assert.Throws<ArgumentException>(() => new Produto(""));
+
+    [Fact]
+    public void DoisProdutos_DevemTerIdsDiferentes_Teste()
+    {
+        var produto1 = new Produto("Produto A");
+        var produto2 = new Produto("Produto B");
+
+        Assert.NotEqual(produto1.Id, produto2.Id);
+    }
+
+    [Fact]
+    public void AlterarNomeProduto_NaoDeveAlterarIdOuDataCriacao_Teste()
+    {
+        var produto = new Produto("Produto A");
+        var idOriginal = produto.Id;
+        var dataCriacaoOriginal = produto.DataCriacao;
+
+        produto.AlterarNomeProduto("Produto Alterado");
+
+        Assert.Equal(idOriginal, produto.Id);
+        Assert.Equal(dataCriacaoOriginal, produto.DataCriacao);
+    }
+
+    [Fact]
+    public void AlterarNomeProduto_DeveAtualizarDataAlteracaoRecente_Teste()
+    {
+        var produto = new Produto("Produto A");
+        produto.AlterarNomeProduto("Produto Alterado");
+
+        Assert.NotNull(produto.DataAlteracao);
+        Assert.InRange(produto.DataAlteracao.Value,
+            DateTime.UtcNow.AddSeconds(-5),
+            DateTime.UtcNow.AddSeconds(5));
+    }
+
+    [Fact]
+    public void Id_DeveSerSomenteLeitura_Teste()
+    {
+        var produto = new Produto("Produto Teste");
+        var property = typeof(Produto).GetProperty("Id");
+        Assert.False(property?.CanWrite);
+    }
+
 }
